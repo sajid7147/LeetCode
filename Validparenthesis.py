823e767ae6a43cf68d32b41954025299974edfec
sajid <117066162+sajid7147@users.noreply.github.com>
2023-10-04 11:47:41 +0530

    Add files via upload

---
+++ Validparenthesis.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isValid(self, s):
+        stack=[]
+        bracket={')':'(','}':'{',']':'['}
+        
+        for char in s:
+            if char in bracket.values():
+                stack.append(char)
+            elif char in bracket.keys():
+                if not stack or stack.pop()!=bracket[char]:
+                    return False
+            else:
+                return False
+            
+        return len(stack)==0
+
+        
