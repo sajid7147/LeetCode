dfbf6f590a667de96de639930262574ec14ce0f5
sajid7147 <sajidtp274@gmail.comm>
2023-10-22 19:42:04 +0530

    Climbing stairs

---
+++ ClimbStairs.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def climbStairs(self, n):
+       if n==0 or n==1:
+           return 1
+
+       ways=[0]*(n+1)
+       ways[0]=1
+       ways[1]=1
+
+       for i in range(2,n+1):
+           ways[i]=ways[i-1]+ways[i-2]
+
+       return ways[n]
