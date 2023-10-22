d9d30c0f7ceeee01ace9236a2af10324a2212621
sajid7147 <sajidtp274@gmail.comm>
2023-10-22 12:48:26 +0530

    Square Root

---
+++ Sqrt.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def mySqrt(self, x):
+        if x==0:
+            return 0
+
+        left,right=1,x
+
+        while left<=right:
+            mid=left+(right-left)//2
+            square=mid*mid
+
+            if square==x:
+                return mid
+            elif square>x:
+                right=mid-1
+            else:
+                left=mid+1
+
+        return right
